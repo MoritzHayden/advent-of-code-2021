bc04eb3e98202319ce15bb4388ce35d4983a14b6
Hayden Moritz <Moritz.Hayden@gmail.com>
2021-12-03 17:53:42 -0600

    Add binary string to int function

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,8 @@ fun readFile(name: String) = File("src/input", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Converts binary string to int
+ */
+fun String.toInt(): Int = Integer.parseInt(this, 2)
