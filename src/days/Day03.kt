7d6f4242c9631310f3c2115a0606bc8e38d3cfb0
Hayden Moritz <Hayden.Moritz@daveramsey.com>
2021-12-03 07:16:45 -0600

    Finish day 3 part 1

--- src/days/Day03.kt
+++ src/days/Day03.kt
@@ -1,5 +1,7 @@
 package days
 
+import readFile
+
 class Day03 {
     fun solveAll() {
         println("Solving day 3...")
@@ -8,7 +10,34 @@ class Day03 {
     }
 
     private fun solvePart1(): Int {
-        return 0
+        val powerConsumption: Int
+        val counter = arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        var gammaRate = ""
+        var epsilonRate = ""
+        val input = readFile("Day03")
+
+        for (i in input.indices) {
+            for (j in 0 until input[i].length) {
+                if (input[i][j] == '1') {
+                    counter[j]++
+                } else {
+                    counter[j]--
+                }
+            }
+        }
+
+        for (i in counter) {
+            if (i > 0) {
+                gammaRate += "1"
+                epsilonRate += "0"
+            } else {
+                gammaRate += "0"
+                epsilonRate += "1"
+            }
+        }
+
+        powerConsumption = Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilonRate, 2)
+        return powerConsumption
     }
 
     private fun solvePart2(): Int {
