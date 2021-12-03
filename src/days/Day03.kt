ab45e1815709d2a8e2a4b81a3007ae710f1b3dc5
Hayden Moritz <Hayden.Moritz@daveramsey.com>
2021-12-03 12:29:11 -0600

    Attempt day 3, part 2

--- src/days/Day03.kt
+++ src/days/Day03.kt
@@ -41,6 +41,67 @@ class Day03 {
     }
 
     private fun solvePart2(): Int {
-        return 0
+        val lifeSupportRating: Int
+        val counter = arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        var oxygenRegex = ""
+        var cO2Regex = ""
+        var oxygenRating = ""
+        var cO2Rating = ""
+        var oxygenFound = false
+        var cO2Found = false
+        val input = readFile("Day03")
+
+        for (i in input.indices) {
+            for (j in 0 until input[i].length) {
+                if (input[i][j] == '1') {
+                    counter[j]++
+                } else {
+                    counter[j]--
+                }
+            }
+        }
+
+        for (i in counter) {
+            if (i >= 0) {
+                oxygenRegex += "1"
+                cO2Regex += "0"
+            } else {
+                oxygenRegex += "0"
+                cO2Regex += "1"
+            }
+        }
+
+        for (i in counter.indices.reversed()) {
+            for (line in input) {
+                if (!oxygenFound && line.matches(oxygenRegex.toRegex())) {
+                    oxygenRating = line
+                    oxygenFound = true
+                }
+                if (!cO2Found && line.matches(cO2Regex.toRegex())) {
+                    cO2Rating = line
+                    cO2Found = true
+                }
+                if (oxygenFound && cO2Found) {
+                    break
+                }
+            }
+            if (oxygenFound && cO2Found) {
+                break
+            } else {
+                if (!oxygenFound) {
+                    oxygenRegex = oxygenRegex.substring(0, i) + '.' + oxygenRegex.substring(i + 1)
+                }
+                if (!cO2Found) {
+                    cO2Regex = cO2Regex.substring(0, i) + '.' + cO2Regex.substring(i + 1)
+                }
+            }
+        }
+
+        println(oxygenRating + " -> " + Integer.parseInt(oxygenRating, 2))
+        println(cO2Rating + " -> " + Integer.parseInt(cO2Rating, 2))
+
+        // Not the following: 866578
+        lifeSupportRating = Integer.parseInt(oxygenRating, 2) * Integer.parseInt(cO2Rating, 2)
+        return lifeSupportRating
     }
 }
