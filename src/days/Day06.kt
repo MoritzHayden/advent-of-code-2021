9291a9ec81e9bbdda57606465620900c301d05f1
Hayden Moritz <Hayden.Moritz@daveramsey.com>
2021-12-06 07:54:42 -0600

    Finish day 6, part 1

--- src/days/Day06.kt
+++ src/days/Day06.kt
@@ -1,5 +1,7 @@
 package days
 
+import readFile
+
 class Day06 {
     fun solveAll() {
         println("Solving day 6...")
@@ -8,10 +10,35 @@ class Day06 {
     }
 
     private fun solvePart1(): Int {
-        return 0
+        val input = readFile("Day06")
+        val fishSchool = parseInput(input)
+
+        return calculatePopulation(fishSchool, 80)
     }
 
     private fun solvePart2(): Int {
-        return 0
+        val input = listOf("3,4,3,1,2") // Sample input
+        // val input = readFile("Day06")
+        val fishSchool = parseInput(input)
+
+        return calculatePopulation(fishSchool, 256)
+    }
+
+    private fun parseInput(input: List<String>): MutableList<Int> {
+        return input[0].split(",").map { it.toInt() }.toMutableList()
+    }
+
+    private fun calculatePopulation(fishSchool: MutableList<Int>, days: Int): Int {
+        for (i in 0 until days) {
+            for (j in 0 until fishSchool.size) {
+                if (fishSchool[j] == 0) {
+                    fishSchool[j] = 6
+                    fishSchool.add(8)
+                } else {
+                    fishSchool[j]--
+                }
+            }
+        }
+        return fishSchool.size
     }
 }
