32b6129801318c564c153148f14dd6726dc50c48
Hayden Moritz <Hayden.Moritz@daveramsey.com>
2021-12-06 12:38:36 -0600

    Complete and refactor day 6

--- src/days/Day06.kt
+++ src/days/Day06.kt
@@ -9,16 +9,15 @@ class Day06 {
         println("Part 2: ${solvePart2()}")
     }
 
-    private fun solvePart1(): Int {
+    private fun solvePart1(): Long {
         val input = readFile("Day06")
         val fishSchool = parseInput(input)
 
         return calculatePopulation(fishSchool, 80)
     }
 
-    private fun solvePart2(): Int {
-        val input = listOf("3,4,3,1,2") // Sample input
-        // val input = readFile("Day06")
+    private fun solvePart2(): Long {
+        val input = readFile("Day06")
         val fishSchool = parseInput(input)
 
         return calculatePopulation(fishSchool, 256)
@@ -28,17 +27,27 @@ class Day06 {
         return input[0].split(",").map { it.toInt() }.toMutableList()
     }
 
-    private fun calculatePopulation(fishSchool: MutableList<Int>, days: Int): Int {
+    private fun calculatePopulation(fishSchool: MutableList<Int>, days: Int): Long {
+        var newFishCount: Long = 0
+        var population = mutableListOf<Long>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+        for (i in 0 .. 8) {
+            population[i] = fishSchool.count{ it == i }.toLong()
+        }
+
         for (i in 0 until days) {
-            for (j in 0 until fishSchool.size) {
-                if (fishSchool[j] == 0) {
-                    fishSchool[j] = 6
-                    fishSchool.add(8)
-                } else {
-                    fishSchool[j]--
+            for (j in population.indices) {
+                if (j == 0) {
+                    newFishCount = population[0]
+                    population[0] = 0
+                }
+                if (j > 0) {
+                    population[j-1] += population[j]
+                    population[j] = 0
                 }
             }
+            population[6] += newFishCount
+            population[8] += newFishCount
         }
-        return fishSchool.size
+        return population.sum()
     }
 }
